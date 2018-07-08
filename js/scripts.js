3fbd2a2b92bca4c9bd4d542c57a2781bb95555dd
IreriVikki <wambsviki@gmail.com>
2018-07-08 12:36:08 +0300

    alerts for valid and invalid email added for the subscribe section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-  $('#lunch').submit(event(){
+  $("#subscribe").click(function(){
+    var email = $("email-entry").val();
+
+    if (email.length < 5){
+      alert("Please enter a valid email");
+    }else {
+      alert("Tommorow's Menu has been sent to your email");
+    }
   });
 });
